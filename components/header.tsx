ce39f6a8744effeabbf48788978f804b5f950d3e
Andrei Bucur <a.bucur@vermeerit.com>
2024-02-17 19:31:50 +0200

    update responive header

--- components/header.tsx
+++ components/header.tsx
@@ -14,13 +14,14 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[45rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] md:w-[45rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          
           {links.slice(0, 3).map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
@@ -60,12 +61,12 @@ export default function Header() {
           ))}
 
           {/* Additional content */}
-          <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+          <div className="hidden md:flex lg:flex xl:w-1/3 xl:justify-center">
             <Link
               href="/"
-              className=" rounded-md p-1  flex items-center justify-center"
+              className="rounded-md p-1 flex items-center justify-center"
             >
-              <span className="text-gray-950 mr-1 text-[1.4rem] font-medium  dark:text-gray-200">
+              <span className="text-gray-950 mr-1 text-[1.4rem] font-medium dark:text-gray-200">
                 bucur
               </span>
               <span className="w-12 h-8 text-[1.4rem] rounded bg-black text-gray-200 dark:text-gray-950 dark:bg-white flex items-center justify-center">
